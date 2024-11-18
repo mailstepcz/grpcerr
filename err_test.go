94998fde82def57def253fff9007ad2867cf220a
Petr Homola <homola.p@gmail.com>
2024-11-18 09:42:54 +0000

    initial commit (#1)

---
+++ err_test.go
@@ -0,0 +1,54 @@
+package grpcerr
+
+import (
+	"database/sql"
+	"errors"
+	"testing"
+
+	"github.com/mailstepcz/serr"
+	"github.com/stretchr/testify/require"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+func TestConvert(t *testing.T) {
+	req := require.New(t)
+
+	dummyErr := Wrap(errors.ErrUnsupported, codes.Unimplemented)
+
+	req.Equal(codes.Unimplemented, status.Code(Convert(dummyErr)))
+
+	req.Equal("unsupported operation", dummyErr.Error())
+
+	req.True(errors.Is(dummyErr, errors.ErrUnsupported))
+
+	req.Equal(codes.NotFound, status.Code(Convert(sql.ErrNoRows)))
+}
+
+func TestConvertWrappedError(t *testing.T) {
+	req := require.New(t)
+
+	dummyErr := serr.Wrap("wrapped", Wrap(errors.ErrUnsupported, codes.Unimplemented))
+
+	req.Equal(codes.Unimplemented, status.Code(Convert(dummyErr)))
+
+	req.Equal("wrapped: unsupported operation", dummyErr.Error())
+
+	req.True(errors.Is(dummyErr, errors.ErrUnsupported))
+
+	req.Equal(codes.NotFound, status.Code(Convert(serr.Wrap("wrapped", sql.ErrNoRows))))
+}
+
+func TestConvertWrappedErrors(t *testing.T) {
+	req := require.New(t)
+
+	dummyErr := errors.Join(errors.New("some error"), Wrap(errors.ErrUnsupported, codes.Unimplemented))
+
+	req.Equal(codes.Unimplemented, status.Code(Convert(dummyErr)))
+
+	req.Equal("some error\nunsupported operation", dummyErr.Error())
+
+	req.True(errors.Is(dummyErr, errors.ErrUnsupported))
+
+	req.Equal(codes.NotFound, status.Code(Convert(errors.Join(errors.New("some error"), sql.ErrNoRows))))
+}
---
+++ go.mod
@@ -0,0 +1,21 @@
+module github.com/mailstepcz/grpcerr
+
+go 1.22.7
+
+toolchain go1.23.3
+
+require (
+	github.com/mailstepcz/serr v0.1.0
+	github.com/stretchr/testify v1.9.0
+	google.golang.org/grpc v1.68.0
+)
+
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/google/uuid v1.6.0 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	golang.org/x/sys v0.25.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20240903143218-8af14fe29dc1 // indirect
+	google.golang.org/protobuf v1.34.2 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
